f18edf009d11e46782af5e3bd6b2af1c85ec949b
Henrique Figueira <henriquefigueira@alunos.utfpr.edu.br>
2019-10-14 11:03:08 -0300

    add more rules for parser

--- compilador/Parser.py
+++ compilador/Parser.py
@@ -38,8 +38,73 @@ class Parser:
         p[0] = Tree('programa', [p[1]])
 
     def p_lista_declaracoes(self, p):
-        'lista_declaracoes : lista_declaracoes', p[[1]]
-        
+        'lista_declaracoes : lista_declaracoes declaracao'
+        p[0] = Tree('lista_declaracoes', [p[1],p[2]])
+
+    def p_lista_declaracoes1(self, p):
+        'lista_declaracoes : lista_declaracoes'
+        p[0] = Tree('lista_declaracoes', [p[1]])
+
+    def p_declaracao(self, p):
+        '''
+            declaracao  :   declaracao_variaveis
+                                |   inicializacao_variaveis
+                                |   declaracao_funcao
+        '''
+        p[0] = Tree('declaracao', [p[1]])
+    
+    def p_declaracao_variaveis(self, p):
+        'declaracao_variaveis : tipo DOIS_PONTOS lista_variaveis'
+        p[0] = Tree('declaracao_variaveis', [p[1],p[3]])
+
+    def p_inicializacao_variaveis(self, p):
+        'inicializacao_variaveis : atribuicao'
+        p[0] = Tree('inicializacao_variaveis', [p[1]])
+
+    def p_lista_variaveis(self, p):
+        'lista_variaveis : lista_variaveis VIRGULA var'
+        p[0] = Tree('lista_variaveis', [p[1], p[3]])
+
+    def p_lista_variaveis1(self, p):
+        'lista_variaveis : var'
+        p[0] = Tree('lista_variaveis', [p[1]])
+
+    def p_var(self, p):
+        'var : ID'
+        p[0] = Tree('var', [], p[1])
+
+    def p_var1(self, p):
+        'var : ID indice'
+        p[0] = Tree('indice', [p[2], p[1]])
+    
+    def p_indice(self, p):
+        'indice : indice ABRE_COL expressao FECHA_COL'
+        p[0] = Tree('indice', [p[1], p[3]])
+
+    def p_indice1(self, p):
+        'indice : ABRE_COL expressao FECHA_COL'
+        p[0] = Tree('indice', [p[2]])
+    
+    def p_tipo(self,p):
+    	'tipo : INTEIRO'
+    	p[0] = Tree('inteiro')  
+   
+    def p_tipo1(self,p):
+    	'tipo : FLUTUANTE'
+    	p[0] = Tree('flutuante')    
+    
+    def p_tipo2(self,p):
+    	'tipo : CIENTIFICO'
+    	p[0] = Tree('cientifico') 
+
+    def p_error(self, p):
+        if p:
+                print("Erro sintático: '%s', linha %d" % (p.value, p.lineno))
+                exit(1)
+        else:
+                #yacc.restart()
+                print('Erro sintático: definições incompletas! ')
+                exit(1)
     
     def seeTree(node, level="\n"):
         if node != None:
