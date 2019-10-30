84c2243f8b049bb7956ec588005ec7fa8d7bb577
Henrique Figueira <henriquefigueira@alunos.utfpr.edu.br>
2019-10-29 21:03:17 -0300

    fixed bugs

--- compilador/Semantica.py
+++ compilador/Semantica.py
@@ -9,25 +9,29 @@ class Semantica:
         self.lex.insertSimbols(code)
         self.table = self.lex.table        
 
-def prefix(root, t):
+def prefix(root, t, escopo):
     if root:
+        if str(root) == "cabecalho":
+            escopo = root.child[0].child[0]
         if str(root) == "declaracao_variaveis":
-            addTipo(root, t)
-        for son in root.child:
-            prefix(son, t)
+            addTipo(root, t, escopo)
 
-def addTipo(node, t):
+        for son in root.child:
+            prefix(son, t, escopo)
+
+def addTipo(node, t, escopo):
     child = node.child[1].child[0].child[0].child[0]
     tipo = node.child[0].child[0]
-    #for
-    print(child)
-    print(tipo)
-
-
+    for simbol in t.table.simbols:
+        if simbol.lexema == str(child):
+            simbol.tipo = tipo
+            simbol.escopo = escopo
 
 if __name__ == '__main__':
     from sys import argv, exit
     a = argv[1].split('/')
     f = open(argv[1])
     t = Semantica(f.read())
-    prefix(t.parser.ast, t)
+    escopo = "global"
+    prefix(t.parser.ast, t, escopo)
+    t.table.tablePrint()
